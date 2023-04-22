19458d196bbf8d33b2b8949b6e31ad57ca7a59d3
Dzim9k <130169801+Dzim9k@users.noreply.github.com>
2023-04-22 20:30:19 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+ // Mobile device hamburger
+ hamburger = document.querySelector(".hamburger");
+
+ hamburger.onclick = function() {
+     navBar = document.querySelector(".nav-bar");
+     navBar.classList.toggle("active");
+     
+     let isactive = document.querySelector(".nav-bar.active");
+     if (isactive) {
+         document.body.style.overflow = 'hidden';
+     }
+     else {
+         document.body.style.overflow = 'auto';
+     }
+ }
+
+ // dropdown menu
+ dropdownMenu = document.querySelector(".nav-dropdown");
+ dropdownMenu.onclick = function() {
+     dropdownContainer = document.querySelector(".nav-dropdown .dropdown");
+     dropdownContainer.classList.toggle("dropdown-active"); 
+ }
+
+ 
+// Zamykanie menu dropdown po kliknięciu gdziekolwiek na stronie
+document.addEventListener('click', function(event) {
+ var dropdownContainer = document.querySelector('.nav-dropdown .dropdown');
+ if (dropdownContainer.classList.contains('dropdown-active') && !event.target.closest('.nav-dropdown')) {
+     dropdownContainer.classList.remove('dropdown-active');
+ }
+});
+
+ const image1 = document.querySelector('.image1');
+ const image2 = document.querySelector('.image2');
+
+ // Zdefiniuj tablicę z linkami do kolejnych zdjęć
+ const images = ['./img/DJI_0276.JPG', './img/karczma1.jpg'];
+ let currentImage = 0;
+
+ // Funkcja do zmiany zdjęcia co 5 sekund
+ function changeImage() {
+     currentImage = (currentImage + 1) % images.length;
+     image1.src = images[currentImage];
+     setTimeout(changeImage, 5000);
+ }
+
+ // Uruchom funkcję po załadowaniu strony
+ window.addEventListener('load', changeImage);
+
+ const menuSection = document.querySelectorAll('.menu-korytko')
+ const menuTabs = document.querySelectorAll('.menu-tab')
+
+ const showInfo = id => {
+
+     menuSection.forEach(section => section.style.display = 'none')
+     menuTabs.forEach(tab => tab.classList.remove('menu-tab-active'))
+
+     document.getElementById(id).style.display = 'block'
+
+     const currentActiveButton = document.querySelector('[data-id=${id}]')
+
+     currentActiveButton.classList.add('menu-tab-active')
+ }
