8a5b494d5896fa1d18b8b274541cd2c133b76b43
Dzim9k <130169801+Dzim9k@users.noreply.github.com>
2023-05-07 20:51:58 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -64,3 +64,26 @@ document.addEventListener('click', function(event) {
 
      currentActiveButton.classList.add('menu-tab-active')
  }
+
+ //Wymiana zdjęć
+ 
+ function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+
+ 
+ 
+ 
+ 
+ 
