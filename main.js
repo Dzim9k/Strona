3ad0f6fe88be0eac42d1f755b64f51b266f5aea6
Dzim9k <130169801+Dzim9k@users.noreply.github.com>
2024-03-25 21:03:12 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -27,3 +27,51 @@ document.addEventListener('click', function (event) {
 		dropdownContainer.classList.remove('dropdown-active')
 	}
 })
+
+//animacja about-us
+document.addEventListener('DOMContentLoaded', event => {
+	const observerOptions = {
+		root: null, // obserwuje względem całego viewportu
+		threshold: 0.1, // callback uruchomi się, gdy co najmniej 10% obserwowanego elementu znajdzie się w obszarze widzenia
+	}
+
+	const observerCallback = (entries, observer) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('visible')
+			} else {
+				entry.target.classList.remove('visible')
+			}
+		})
+	}
+
+	const observer = new IntersectionObserver(observerCallback, observerOptions)
+
+	// Dodaj obserwowanie dla .content-wrapper i .content-image
+	document.querySelectorAll('.content-wrapper, .content-image').forEach(el => {
+		observer.observe(el)
+	})
+})
+
+document.querySelectorAll('.menu-category').forEach(button => {
+	button.addEventListener('click', function () {
+		// Usuwanie klasy 'active' z wszystkich przycisków
+		document.querySelectorAll('.menu-category').forEach(btn => {
+			btn.classList.remove('active')
+		})
+		// Dodanie klasy 'active' do klikniętego przycisku
+		this.classList.add('active')
+	})
+})
+
+function changeBackground(image, textColor) {
+	document.body.style.backgroundImage = `url('${image}')`
+	document.body.style.backgroundSize = 'cover' // Pokrycie całego tła obrazem
+	document.body.style.color = textColor // Ustawienie koloru tekstu dla body
+
+	// Ustawienie koloru tekstu dla przycisków w menuCategories
+	var menuButtons = document.querySelectorAll('#menuCategories .menu-category')
+	menuButtons.forEach(function (button) {
+		button.style.color = textColor
+	})
+}
